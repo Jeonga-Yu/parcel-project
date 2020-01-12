fd236ce018469569ff192c2ade21cd80c5a0df8d
devJ3rry <dev.j3rry@gmail.com>
2020-01-12 23:51:09 +0900

    Initail Update

---
+++ App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    hello world!
+    <i class="fab fa-500px"></i>
+  </div>
+  
+</template>
+<script>
+import Vue from 'Vue'
+// import FontAwesomeIcon from '@fontawesome/vue-fontawesome'
+
+export default Vue.extend({
+  data() {
+    return {
+      bundler: "Parcel"
+    }
+  }
+})
+</script>
+<style lang="scss" scoped>
+.container {
+  color: green;
+}
+</style>
